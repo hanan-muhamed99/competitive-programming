4e05ac6d922c301f43557306a76ae17dd65afc97
Hanan-AbdElsalam193 <hananabdelsalam611@gmail.com>
2021-11-23 23:34:57 +0200

    first program

---
+++ hackerrank/warmup/solve_me_first.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS                         \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cout.tie(NULL);
+
+#define FIO freopen("input.txt", "r", stdin);
+
+int main() {
+  IOS;
+  //   FIO;
+
+  int a, b;
+  scanf("%d%d", &a, &b);
+  printf("%d\n", a + b);
+
+  return 0;
+}
